f8d25f351a10a63c868728a42fb0e346f0c40801
BulatovaTati <kohodatat@gmail.com>
2022-10-07 02:17:12 +0300

    add info in readme

--- src/js/common/common-index.js
+++ src/js/common/common-index.js
@@ -1,2 +1,3 @@
 import './onEscClose';
 import './refs';
+import './pagination';
--- src/js/dom/dom-index.js
+++ src/js/dom/dom-index.js
@@ -1,6 +1,4 @@
-import './render-movie-cards';
 import './scrollToTop';
-import './pagination';
 import './movie-modal';
 import './input-search';
 import './queue';
