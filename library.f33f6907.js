f8606db3df654861d8b2ac231e135b85b1670e48
BulatovaTati <102695106+BulatovaTati@users.noreply.github.com>
2022-10-07 11:00:55 +0000

    Deploying to gh-pages from @ BulatovaTati/my__filmoteka@d211f769b279947bfd7e5a7011bbc6c6b8dc3832 🚀

---
+++ library.f33f6907.js
@@ -0,0 +1,2 @@
+document.querySelector("#header"),document.querySelector(".cards__list"),document.querySelector(".footer"),document.querySelector("#search-list"),document.querySelector(".pagination");
+//# sourceMappingURL=library.f33f6907.js.map
