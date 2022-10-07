3ee802b25e590e5eae805764b1dcbb8ec7c91018
BulatovaTati <102695106+BulatovaTati@users.noreply.github.com>
2022-10-07 13:34:10 +0000

    Deploying to gh-pages from @ BulatovaTati/my__filmoteka@3dabb514be96c7da85785083cd6b4a363eb23919 🚀

--- library.f33f6907.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#header"),document.querySelector(".cards__list"),document.querySelector(".footer"),document.querySelector("#search-list"),document.querySelector(".pagination");
-//# sourceMappingURL=library.f33f6907.js.map
