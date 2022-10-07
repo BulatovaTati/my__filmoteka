4373e7efd92bbdc86be68f014ca10e4007c6f8c5
BulatovaTati <kohodatat@gmail.com>
2022-10-07 19:59:36 +0300

    Update apiService.js

--- src/js/api/apiService.js
+++ src/js/api/apiService.js
@@ -1,29 +1,117 @@
 import axios from 'axios';
 export { API_KEY, BASE_URL } from './api-vars';
 
-// Посилання на документацію для запиту на список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці:
-async function fetchGetTrending(pageValue) {
-  const { data } = await axios.get(
-    `/trending/movie/week?api_key=${API_KEY}&page=${pageValue}`
-  );
-  const { results, total_pages, page, total_results } = data;
-  return { results, total_pages, page, total_results };
+export default class MoviesApiService {
+  constructor() {
+    this.page = 1;
+    this.searchQuery = '';
+    this.lang = '';
+    this.genre = '';
+    this.year = '';
+    this.originalLanguage = '';
+    this.vote = '';
+  }
+
+  async getPopularFilms() {
+    try {
+      const url = `${BASE_URL}movie/popular?api_key=${API_KEY}&language=${this.lang}&page=${this.page}`;
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getTrendFilms() {
+    try {
+      const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}&language=${this.lang}`;
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getFilmsByName() {
+    try {
+      const searchParams = new URLSearchParams({
+        api_key: '7c47f39dc565b202f275767eff95366e',
+        query: this.searchQuery,
+        language: 'en-US',
+        page: this.page,
+        include_adult: false,
+      });
+      const url = `${BASE_URL}search/movie?${searchParams}`;
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getFilmDetails(id) {
+    try {
+      const url = `${BASE_URL}movie/${id}?api_key=${API_KEY}&language=${this.lang}`;
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getFilmVideo(id) {
+    try {
+      const url = `${BASE_URL}movie/${id}/videos?api_key=${API_KEY}&language=${this.lang}`;
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getFilteredMovies() {
+    try {
+      const searchParams = new URLSearchParams({
+        api_key: '7c47f39dc565b202f275767eff95366e',
+        language: this.lang,
+        sort_by: 'popularity.desc',
+        page: this.page,
+        include_adult: false,
+        with_genres: this.genre,
+        primary_release_year: this.year,
+        with_original_language: this.originalLanguage,
+      });
+      const url = `${BASE_URL}discover/movie?${searchParams}&vote_average.gte=${this.vote}`;
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  setLang(newLang) {
+    this.lang = newLang;
+  }
+
+  getLang() {
+    return this.lang;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+  decrementPage() {
+    this.page -= 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
 }
-// Посилання на документацію для запиту фільму за ключовим словом на головній сторінці:
-async function fetchGetSearchMovie(valueInput, pageValue) {
-  const { data } = await axios.get(
-    `/search/movie?api_key=${API_KEY}&page=${pageValue}&query=${valueInput}`
-  );
-  const { results, total_pages, page, total_results } = data;
-  return { results, total_pages, page, total_results };
-}
-
-// async function fetchGetMovieById(id) {
-//   const { data } = await axios.get(`/movie/${id}?api_key=${API_KEY}`);
-
-//   return data;
-// }
-
-// ! Посилання на документацію для запиту повної інформації про кінофільм для сторінки кінофільму: https://developers.themoviedb.org/3/movies/get-movie-details
-
-// export { fetchGetSearchMovie, fetchGetSearchMovie };
