8a546ec9f78080a1b1032e8c4cb211a6c06cce42
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2022-10-08 10:12:09 +0300

    add pagination lib

--- src/js/common/pagination.js
+++ src/js/common/pagination.js
@@ -1,8 +1,38 @@
-const refs = {
-  pagination: document.querySelector('.pagination'),
+import Pagination from 'tui-pagination';
+
+const options = {
+  totalItems: 6000,
+  itemsPerPage: 6,
+  visiblePages: 6,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 
-let pages = 1000;
-let currentPage = 1;
+const pagination = new Pagination('pagination', options);
 
-async function onPaginationClick(params) {}
+const refs = {
+  prevBtn: document.querySelector('.tui-ico-prev'),
+  nextBtn: document.querySelector('.tui-next'),
+};
+
+// refs.prevBtn.textContent = '<';
+// refs.nextBtn.textContent = '>';
