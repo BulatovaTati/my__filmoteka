eccff36187c4066bb05891fe289c9832a8ef6bb0
Oleksii26 <Kolesnik26@ukr.net>
2022-10-08 19:34:17 +0300

    second-save

--- src/js/dom/queue.js
+++ src/js/dom/queue.js
@@ -1,47 +1,47 @@
 
-const films1 = [
-    {
-        "title": "Terminator 3",
-        "rating": 8.9,
-        "img": 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-        'opis': 'this is american film'
-    },
-    {
-        "title": "Rambo 3",
-        "rating": 7.9,
-        "img": 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
-        'opis': 'this is Australian film'
-    }
-]
+// const films1 = [
+//     {
+//         "title": "Terminator 3",
+//         "rating": 8.9,
+//         "img": 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
+//         'opis': 'this is american film'
+//     },
+//     {
+//         "title": "Rambo 3",
+//         "rating": 7.9,
+//         "img": 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
+//         'opis': 'this is Australian film'
+//     }
+// ]
 
 
-const conQ = films1[0].name || films1[0].title
+// const conQ = films1[0].name || films1[0].title
 
-queue.addEventListener('click', addQueue)
-
-
-localStorage.getItem('filmQ', JSON.stringify(conQ) || []).includes(conQ) ? queue.classList.add('delete_video') : queue.classList.add('added_video')
+// queue.addEventListener('click', addQueue)
 
 
+// localStorage.getItem('filmQ', JSON.stringify(conQ) || []).includes(conQ) ? queue.classList.add('delete_video') : queue.classList.add('added_video')
 
-function addQueue(e) {
 
-    if (conQ) {
-        let filmQ = JSON.parse(localStorage.getItem('filmQ')) || []
 
-        if (filmQ.includes(conQ)) {
+// function addQueue(e) {
 
-            queue.classList.remove('delete_video')
-            queue.classList.add('added_video')
-            filmQ = filmQ.filter(eId => eId !== conQ)
-        } else {
+//     if (conQ) {
+//         let filmQ = JSON.parse(localStorage.getItem('filmQ')) || []
 
-            queue.classList.add('delete_video')
-            queue.classList.remove('added_video')
-            filmQ.push(conQ)
-        }
+//         if (filmQ.includes(conQ)) {
 
-        localStorage.setItem('filmQ', JSON.stringify(filmQ))
-    }
+//             queue.classList.remove('delete_video')
+//             queue.classList.add('added_video')
+//             filmQ = filmQ.filter(eId => eId !== conQ)
+//         } else {
 
-}
+//             queue.classList.add('delete_video')
+//             queue.classList.remove('added_video')
+//             filmQ.push(conQ)
+//         }
+
+//         localStorage.setItem('filmQ', JSON.stringify(filmQ))
+//     }
+
+// }
--- src/js/dom/watched.js
+++ src/js/dom/watched.js
@@ -1,53 +1,53 @@
  
-const films = [
-    {
- "title" : "Terminator 3",
- "rating" : 8.9,
- "img" : 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
- 'opis' : 'this is american film'
-     },
-  { 
-  "title" : "Rambo 3",
-  "rating" : 7.9,
-  "img" : 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
-  'opis' : 'this is Australian film'
- }
-   ]  
+// const films = [
+//     {
+//  "title" : "Terminator 3",
+//  "rating" : 8.9,
+//  "img" : 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
+//  'opis' : 'this is american film'
+//      },
+//   { 
+//   "title" : "Rambo 3",
+//   "rating" : 7.9,
+//   "img" : 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
+//   'opis' : 'this is Australian film'
+//  }
+//    ]  
  
-   const con = films[1].name || films[1].title
- 
- 
- const card = document.querySelector('.card')
- const watch = document.querySelector('[data-action="watch"]')
- const queue = document.querySelector('[data-action="queue"]')
+//    const con = films[1].name || films[1].title
  
  
- 
- watch.addEventListener('click', addWatch)
- 
- 
- localStorage.getItem('film', JSON.stringify(con) || []).includes(con)?watch.classList.add('delete_video'):watch.classList.add('added_video')
+//  const card = document.querySelector('.card')
+//  const watch = document.querySelector('[data-action="watch"]')
+//  const queue = document.querySelector('[data-action="queue"]')
  
  
- function addWatch() {
  
-  if(con) {
-      let film = JSON.parse(localStorage.getItem('film')) || []
+//  watch.addEventListener('click', addWatch)
+ 
+ 
+//  localStorage.getItem('film', JSON.stringify(con) || []).includes(con)?watch.classList.add('delete_video'):watch.classList.add('added_video')
+ 
+ 
+//  function addWatch() {
+ 
+//   if(con) {
+//       let film = JSON.parse(localStorage.getItem('film')) || []
   
  
-   if(film.includes(con)) {
+//    if(film.includes(con)) {
  
-     watch.classList.remove('delete_video')
-     watch.classList.add('added_video')
-        film = film.filter(eId => eId !== con)
-   } else {
+//      watch.classList.remove('delete_video')
+//      watch.classList.add('added_video')
+//         film = film.filter(eId => eId !== con)
+//    } else {
     
-     watch.classList.add('delete_video')
-     watch.classList.remove('added_video')
-     film.push(con)
-   }
+//      watch.classList.add('delete_video')
+//      watch.classList.remove('added_video')
+//      film.push(con)
+//    }
  
-  localStorage.setItem('film', JSON.stringify(film))
-  }
+//   localStorage.setItem('film', JSON.stringify(film))
+//   }
   
- }
+//  }
