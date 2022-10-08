2b1ff28ffc7801ed9920aa3f118750ceec461ccd
BulatovaTati <kohodatat@gmail.com>
2022-10-08 22:50:27 +0300

    add swiper

---
+++ src/js/other/swiper.js
@@ -0,0 +1,18 @@
+// import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.css';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  centeredSlides: true,
+  // speed: 500,
+
+  // autoplay: {
+  //   delay: 3500,
+  //   disableOnInteraction: false,
+  // },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
