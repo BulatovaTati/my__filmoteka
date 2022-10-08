0c742ffea6ad0bc307965a1f29f7aec2d721c85e
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2022-10-08 21:23:30 +0300

    ressponse pagination/change template

--- src/js/common/pagination.js
+++ src/js/common/pagination.js
@@ -1,38 +1,10 @@
 import Pagination from 'tui-pagination';
-
-const options = {
-  totalItems: 6000,
-  itemsPerPage: 6,
-  visiblePages: 6,
-  page: 1,
-  centerAlign: true,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-};
+import { options } from './options-pagination';
 
 const pagination = new Pagination('pagination', options);
 
-const refs = {
-  prevBtn: document.querySelector('.tui-ico-prev'),
-  nextBtn: document.querySelector('.tui-next'),
-};
+pagination.on('afterMove', paginationScrollTop);
 
-// refs.prevBtn.textContent = '<';
-// refs.nextBtn.textContent = '>';
+function paginationScrollTop(e) {
+  console.log(e);
+}
