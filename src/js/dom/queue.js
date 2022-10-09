c1730fdb87a358f163eeb4956ef21c34c27032f5
vitalik poliakov <vitalik211287@gmail.com>
2022-10-09 07:38:37 +0300

    buttons eithout style

--- src/js/dom/queue.js
+++ src/js/dom/queue.js
@@ -1,47 +1,65 @@
+const settings = [
+  {
+    title: 'Terminator 3',
+    vote_average: 8.911,
+    img: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
+    release_date: '2022.12.20',
+    genre_ids: 'ection',
+  },
+  {
+    title: 'Rambo 3',
+    vote_average: 7.922,
+    img: 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
+    release_date: '2022.12.20',
+    genre_ids: 'ection',
+  },
+];
 
-// const films1 = [
-//     {
-//         "title": "Terminator 3",
-//         "rating": 8.9,
-//         "img": 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-//         'opis': 'this is american film'
-//     },
-//     {
-//         "title": "Rambo 3",
-//         "rating": 7.9,
-//         "img": 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
-//         'opis': 'this is Australian film'
-//     }
-// ]
+import { refs } from '../common/refs';
 
+const queueButton = document.querySelector('.queue_button');
 
-// const conQ = films1[0].name || films1[0].title
+queueButton.addEventListener('click', handleClickQuery);
 
-// queue.addEventListener('click', addQueue)
+function handleClickQuery() {
+  getArreyFromlocalStorage('settings');
+}
 
+function getArreyFromlocalStorage(name) {
+  const addedFilms = localStorage.getItem(name);
+  if (addedFilms) {
+    try {
+      parsedaddedFilms = JSON.parse(addedFilms);
+      cardRender(parsedaddedFilms);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    alert('Вибачте, жоднен фільм не додано');
+  }
+}
 
-// localStorage.getItem('filmQ', JSON.stringify(conQ) || []).includes(conQ) ? queue.classList.add('delete_video') : queue.classList.add('added_video')
+function cardRender(result) {
+    result.map(el  => {
+        console.log( el ); 
+    year = el.release_date.slice(0, 4);
+    src = el.poster_path;
+    rating = el.vote_average.toFixed(1);
+    genre = el.genre_ids;
 
+    refs.cardsContainer.insertAdjacentHTML(
+      'afterbegin',
+      `<li class="cards__item ">    
+        <img src="${el.img}" alt="Movie" class="movie-card__img">
+        <div class="movie-card__info">
+          <h2 class="movie-card__info-title">${el.title}</h2>
+          <p class="movie-card__info">Genre<span> | </span>${el.genre}</p>
+          <p class="movie-card__info">${rating}</p>
+          <p class="movie-card__info">${year}</p>
+        </div>
+      </li>`
+    );
+  });
+}
 
-
-// function addQueue(e) {
-
-//     if (conQ) {
-//         let filmQ = JSON.parse(localStorage.getItem('filmQ')) || []
-
-//         if (filmQ.includes(conQ)) {
-
-//             queue.classList.remove('delete_video')
-//             queue.classList.add('added_video')
-//             filmQ = filmQ.filter(eId => eId !== conQ)
-//         } else {
-
-//             queue.classList.add('delete_video')
-//             queue.classList.remove('added_video')
-//             filmQ.push(conQ)
-//         }
-
-//         localStorage.setItem('filmQ', JSON.stringify(filmQ))
-//     }
-
-// }
+export { getArreyFromlocalStorage };
