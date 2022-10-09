8237f93e25cd9daef8d021e9146156bb63f1e6e9
BulatovaTati <kohodatat@gmail.com>
2022-10-09 22:13:41 +0300

    add function trending

---
+++ src/js/dom/reanderTranding.js
@@ -0,0 +1,16 @@
+import { getPopularData } from '../api/fetchApi';
+import { renderCollection } from './create-movie-markup';
+
+const page = 1;
+
+// getPopularData(page)
+//   .then(data => {
+//     renderCollection(data.results);
+//   })
+//   .catch(console.log);
+
+window.addEventListener('load', () => {
+  getPopularData(page).then(data => {
+    renderCollection(data.results);
+  });
+});
