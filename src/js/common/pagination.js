388f8c6a7d074269bcf293461929d6a129cd35fa
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2022-10-10 16:26:30 +0300

    add popular fetch

--- src/js/common/pagination.js
+++ src/js/common/pagination.js
@@ -1,13 +1,39 @@
 import Pagination from 'tui-pagination';
+import { getPopularData } from '../api/fetchApi';
 import { options } from './options-pagination';
 import Spinner from './spinner';
+
+export {
+  getPopularData,
+  fetchMovieSearcher,
+  fetchMovieForId,
+  getGenres,
+} from '../api/fetchApi';
 
 const pagination = new Pagination('pagination', options);
 const spinner = new Spinner();
 
 pagination.on('afterMove', onPaginationClick);
 
-function onPaginationClick(e) {
-  console.log(e);
+async function onPaginationClick(e) {
+  const selectedPage = e.page;
+
   spinner.enable();
+  const data = await getPopularData(selectedPage);
+  onTopScroll();
+
+  setTimeout(() => {
+    spinner.disable();
+  }, 400);
+
+  // console.log(data);
+  // options.totalItems = 100;
+  // console.log(options.totalItems);
+}
+
+function onTopScroll() {
+  window.scrollTo({
+    top: 240,
+    behavior: 'smooth',
+  });
 }
