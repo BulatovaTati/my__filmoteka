a76d25a53d1151223f1098cb9970eb2e72b54fd8
BulatovaTati <kohodatat@gmail.com>
2022-10-10 14:04:24 +0300

    change imports

--- src/index-library.js
+++ src/index-library.js
@@ -1,1 +1,15 @@
-// import './sass/index.scss';
+import './sass/index.scss';
+// Api
+import './js/api/api-index';
+
+// common
+import './js/common/common-index';
+
+// Dom
+import './js/dom/changeGenresToName';
+import './js/dom/create-movie-markup';
+import './js/dom/create-movie-modal-markup';
+import './js/dom/modal-footer';
+import './js/dom/my_library';
+import './js/dom/scrollToTop';
+import './js/other/other-index';
--- src/js/dom/dom-index.js
+++ src/js/dom/dom-index.js
@@ -1,7 +1,8 @@
 import './scrollToTop';
 import './modal-footer';
 import './input-search';
-import './my_library';
+// import './my_library';
 import './create-movie-markup';
 import './create-movie-modal-markup';
 import './changeGenresToName';
+import './reanderTranding';
