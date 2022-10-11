4c8441d3a721bb79d489e2aa259abeefa9b75eab
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2022-10-11 15:14:07 +0300

    dev-error message fix
    dev-error message fix

--- src/js/dom/input-search.js
+++ src/js/dom/input-search.js
@@ -3,31 +3,47 @@ import { fetchMovieSearcher, getGenres } from '../api/fetchApi';
 import { renderCollection } from './create-movie-markup';
 import { addToStorage } from '../other/localeStorageServices';
 import Spinner from '../common/spinner';
-// refs.search_form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   const value = e.currentTarget.searchQuery.value.trim();
-//   console.log(value);
-//   if (value === '') {
-//     refs.input_error.classList.remove('visually-hidden');
-//     setTimeout(() => {
-//       refs.input_error.classList.add('visually-hidden');
-//     }, 3000);
-//     return;
-//   }
-//   fetchMovieSearcher(value)
-//     .then(data => {
-//       renderCollection(data.results);
-//       console.log(data.results);
-//     })
-//     .catch(console.log);
-// });
 
-// getGenres().then(data => {
-//   addToStorage('allGenres', data.genres);
-// });
 const spinner = new Spinner();
 
 refs.search_form.addEventListener('submit', onFormSubmit);
+
+// async function onFormSubmit(e) {
+//   e.preventDefault();
+//   const value = e.currentTarget.searchQuery.value.trim();
+//   spinner.enable();
+//   console.log(value);
+//   if (!value) {
+//     refs.input.value = '';
+//     refs.input_error.classList.add('visually-show-error');
+//     setTimeout(() => {
+//       refs.input_error.classList.remove('visually-show-error');
+//     }, 3000);
+//     spinner.disable();
+//     return;
+//   }
+
+//   try {
+//     const response = await fetchMovieSearcher(value);
+//     const movieArr = response.results;
+
+//     if (movieArr.length === 0) {
+//       refs.input.value = '';
+//       refs.input_error.classList.add('visually-show-error');
+//       setTimeout(() => {
+//         refs.input_error.classList.remove('visually-show-error');
+//       }, 3000);
+//       spinner.disable();
+//       return;
+//     }
+//     addToStorage('input-value', value);
+//     spinner.disable();
+//     renderCollection(movieArr);
+//     refs.input.value = '';
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 async function onFormSubmit(e) {
   e.preventDefault();
@@ -37,8 +53,11 @@ async function onFormSubmit(e) {
   if (!value) {
     refs.input.value = '';
     refs.input_error.classList.add('visually-show-error');
+    refs.input_error.textContent = 'Please enter a movie name to search';
+
     setTimeout(() => {
       refs.input_error.classList.remove('visually-show-error');
+      refs.input_error.textContent = '';
     }, 3000);
     spinner.disable();
     return;
@@ -50,8 +69,12 @@ async function onFormSubmit(e) {
 
     if (movieArr.length === 0) {
       refs.input.value = '';
+      refs.input_error.textContent = 'Please enter a movie name to search';
+
       refs.input_error.classList.add('visually-show-error');
       setTimeout(() => {
+        refs.input_error.textContent = '';
+
         refs.input_error.classList.remove('visually-show-error');
       }, 3000);
       spinner.disable();
