289d17cd0639f5eae5368d54a166dad0c0507558
vitalik poliakov <vitalik211287@gmail.com>
2022-10-12 00:26:02 +0300

    alarm message

--- src/js/dom/my_library.js
+++ src/js/dom/my_library.js
@@ -1,38 +1,27 @@
-// settings = [
-//   {
-//     title: 'Terminator 3',
-//     vote_average: 8.28,
-//     img: 'https://upload.wikimedia.org/wikipedia/ru/c/ca/Terminator_poster.jpg',
-//     release_date: '2022.12.20',
-//     genre_ids: [28, 36, 99],
-//   },
-//   {
-//     title: 'Rambo 3',
-//     vote_average: 7.945,
-//     img: 'https://m.media-amazon.com/images/M/MV5BODBmOWU2YWMtZGUzZi00YzRhLWJjNDAtYTUwNWVkNDcyZmU5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
-//     first_air_date: '2022.12.20',
-//     genre_ids: [35, 14, 53],
-//   },
-// ];
-
-// localStorage.setItem('settings', JSON.stringify(settings));
-
 import { refs } from '../common/refs';
 import { getFromStorage } from '../other/localeStorageServices';
 
 const queueButton = document.querySelector('.queue_button');
 const watchedButton = document.querySelector('.watched_button');
+const cardsAlert = document.querySelector('.cards');
+const cardsList = document.querySelector('.cards__list');
+
 
 watchedButton.addEventListener('click', handleClickWatched);
-
 queueButton.addEventListener('click', handleClickQueue);
 
 function handleClickQueue() {
-  renderSavedFilms('settings');
+  deletedMessage();
+  renderSavedFilms('queue');
+  removeDisabled(watchedButton);
+  setDisabled(queueButton);
 }
 
 function handleClickWatched() {
+  deletedMessage();
   renderSavedFilms('settings');
+  setDisabled(watchedButton);
+  removeDisabled(queueButton);
 }
 
 function renderSavedFilms(name) {
@@ -40,7 +29,12 @@ function renderSavedFilms(name) {
   if (addedFilms && addedFilms.length > 0) {
     cardRender(addedFilms);
   } else {
-    alert('Вибачте, жоднен фільм не додано');
+    const stringAlert = `<div class="alert__mеssаge">
+      <span class="sorry"> Sorry :(</span>
+      <p class="string__alert"> No movies have been added yet</p>
+      </div>
+      `;
+    cardsAlert.insertAdjacentHTML('afterbegin', stringAlert);
   }
 }
 
@@ -55,20 +49,20 @@ function cardRender(result) {
       const filmGenresArray = el.genre_ids.map(id => {
         return genresObj[id].name;
       });
-        const title = el.name || el.title;
+      const title = el.name || el.title;
       const year =
         el.release_date?.slice(0, 4) || el.first_air_date?.slice(0, 4);
       const src = el.poster_path;
       const rating = el.vote_average.toFixed(1);
       const genre = filmGenresArray.join(', ');
       return `<li class="cards__item ">    
-        <img src="https://image.tmdb.org/t/p/w500${src}" alt="Movie" class="movie-card__img">
-        <div class="movie-card__info">
-          <h2 class="movie-card__info-title">${title}</h2>
-         <div class="movie-card_about">  
-          <p class="movie-card__info-short">Genre<span> | </span>${genre} </p>
-           <p class="movie-card__info-short">${year}</p>
-          <p class="movie-card__info-rating">${rating}</p>
+        <img src="https://image.tmdb.org/t/p/w500${src}" alt="Movie" class="card__img">
+        <div class="card__info">
+          <h2 class="card__title">${title}</h2>
+         <div class="card__decr">  
+          <p class="card__genre">${genre} <span> | </span> </p>
+           <p class="card__year">${year}</p>
+          <p class="card__rating">${rating}</p>
           </div>
         </div>
       </li>`;
@@ -76,4 +70,15 @@ function cardRender(result) {
     .join('');
 
   refs.cardsContainer.insertAdjacentHTML('afterbegin', cardListString);
+}
+
+function setDisabled(el) {
+  el.setAttribute('disabled', '');
+}
+function removeDisabled(el) {
+  el.removeAttribute('disabled');
+}
+function deletedMessage() {
+  cardsList.innerHTML = '';
+  cardsAlert.firstChild.innerHTML = '';
 }
