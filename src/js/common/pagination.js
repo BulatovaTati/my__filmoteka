081f190353e96d17d480c707a4deb9235e35b871
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2022-10-12 17:01:01 +0300

    pagination fix

--- src/js/common/pagination.js
+++ src/js/common/pagination.js
@@ -1,38 +1,37 @@
 import Pagination from 'tui-pagination';
-import { getPopularData } from '../api/fetchApi';
-import { options } from './options-pagination';
 import Spinner from './spinner';
+import { getPopularData, fetchMovieSearcher } from '../api/fetchApi';
+import { options } from './options-pagination';
 import { renderCollection } from '../dom/create-movie-markup';
-
-export {
-  getPopularData,
-  fetchMovieSearcher,
-  fetchMovieForId,
-  getGenres,
-} from '../api/fetchApi';
 
 const pagination = new Pagination('pagination', options);
 const spinner = new Spinner();
 
 pagination.on('afterMove', onPaginationClick);
 
-async function onPaginationClick(e) {
+function onPaginationClick(e) {
   const selectedPage = e.page;
+  const inputValue = localStorage.getItem('input-value');
 
   spinner.enable();
-  const data = await getPopularData(selectedPage);
 
-  const render = await renderCollection(data.results);
-  setTimeout(() => {
-    onTopScroll();
-  }, 400);
+  if (inputValue) {
+    const data = fetchMovieSearcher(inputValue, selectedPage);
+
+    renderCollection(data.results);
+  } else {
+    const data = getPopularData(selectedPage);
+
+    renderCollection(data.results);
+  }
 
   setTimeout(() => {
+    scrollToTop();
     spinner.disable();
   }, 400);
 }
 
-function onTopScroll() {
+function scrollToTop() {
   window.scrollTo({
     top: 240,
     behavior: 'smooth',
