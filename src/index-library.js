e73f787c5d43541a44b54dd3bb475e11521acb89
vitalik poliakov <vitalik211287@gmail.com>
2022-10-12 23:15:00 +0300

    fixed footer position

--- src/index-library.js
+++ src/index-library.js
@@ -12,10 +12,3 @@ import './js/dom/modal-footer';
 import './js/dom/my_library';
 import './js/dom/scrollToTop';
 import './js/other/other-index';
-// const doc = document.querySelector('.header__library');
-
-// if (doc) {
-//   const footer = document.querySelector('.footer');
-//   footer.style.position = 'absolute';
-//   footer.style.bottom = '0';
-// }
