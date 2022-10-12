83cca6e4769448e058fcdd640e901e2b455c3312
Oleksandr Chyhryn <as.chigrin@gmail.com>
2022-10-12 13:27:58 +0300

    Виправляє стилі модалки та рендерінг модалки при кліку на картку фільма

--- src/js/dom/modal-movie.js
+++ src/js/dom/modal-movie.js
@@ -1,44 +1,72 @@
-const cardsList = document.querySelector('.cards__list');
-const modalMovie = document.querySelector('.modal-movie');
-const backdrop = document.querySelector('.backdrop');
+import { fetchMovieForId } from '../api/fetchApi';
 
-cardsList.addEventListener('click', onCardClick);
+setTimeout(() => {
+  const cardsList = document.querySelector('.cards__list');
+  const modalMovie = document.querySelector('.modal-movie');
+  const backdrop = document.querySelector('.backdrop');
+  const cardsItem = cardsList.querySelectorAll('li');
 
-function onCardClick(e) {
-  e.preventDefault();
-  if (e.target !== cardsList) {
-    modalMovieToggle();
-    renderMovieInfo();
-    backdrop.addEventListener('click', onCloseModal);
-    window.addEventListener('keydown', onCloseModal);
-    modalMovie.addEventListener('click', onCloseModal);
+  let movieData = {};
+
+  cardsItem.forEach(elem => {
+    elem.addEventListener('click', onCardClick);
+  });
+
+  function onCardClick(e) {
+    e.preventDefault();
+    document.body.classList.add('modal-open');
+    fetchMovieForId(e.currentTarget.dataset.id)
+      .then(response => {
+        movieData = response;
+
+        modalMovieToggle();
+        renderMovieInfo(response);
+        addModalListeners();
+      })
+      .catch(error => console.log(error));
   }
-}
 
-function modalMovieToggle() {
-  backdrop.classList.toggle('is-hidden');
-  modalMovie.classList.toggle('is-hidden');
-}
-
-function onCloseModal(e) {
-  e.preventDefault();
-  const isContainsClass = e.target.classList.contains('close-btn__icon');
-
-  if (e.key === 'Escape' || isContainsClass || e.target === backdrop) {
-    modalMovieToggle();
-    clearMovieInfo();
-    window.removeEventListener('keydown', onCloseModal);
-    backdrop.removeEventListener('click', onCloseModal);
-    modalMovie.removeEventListener('click', onCloseModal);
+  function modalMovieToggle() {
+    backdrop.classList.toggle('is-hidden');
+    modalMovie.classList.toggle('is-hidden');
   }
-}
 
-function clearMovieInfo() {
-  modalMovie.innerHTML = '';
-}
+  function onCloseModal(e) {
+    e.preventDefault();
+    const isContainsClass =
+      e.target.classList.contains('close-btn__icon') ||
+      e.target.parentNode.classList.contains('close-btn__icon');
 
-function renderMovieInfo() {
-  modalMovie.innerHTML = `<button class="btn close-btn" type="button" data-modal-close>
+    if (
+      e.code === 'Escape' ||
+      isContainsClass ||
+      e.target === e.currentTarget
+    ) {
+      modalMovieToggle();
+      clearMovieInfo();
+      removeModalListeners();
+      document.body.classList.remove('modal-open');
+    }
+  }
+
+  function clearMovieInfo() {
+    modalMovie.innerHTML = '';
+  }
+
+  function renderMovieInfo({
+    poster_path,
+    title,
+    genres,
+    popularity,
+    vote_count,
+    vote_average,
+    original_title,
+    overview,
+  }) {
+    const ganresName = genres.map(genre => genre.name).join(', ');
+    const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+
+    modalMovie.innerHTML = `<button class="btn close-btn" type="button">
     <svg class="close-btn__icon" width="30" height="30" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
       <path d="M23.733 10.304l-1.504-1.504-5.963 5.963-5.963-5.963-1.504 1.504 5.963 5.963-5.963 5.963 1.504 1.504 5.963-5.963 5.963 5.963 1.504-1.504-5.963-5.963 5.963-5.963z"></path>
     </svg>
@@ -46,12 +74,12 @@ function renderMovieInfo() {
   <div class="modal-movie__thumb">
     <img
       class="modal-movie__img"
-      src="https://cdn.pixabay.com/photo/2022/08/22/10/01/tree-7403295_1280.jpg"
-      alt=""
+      src="${BASE_IMG_URL + poster_path}"
+      alt="${original_title + ' poster'}"
     />
   </div>
   <div class="modal-movie__info-container">
-    <h2 class="modal-movie__title">A fistful of lead</h2>
+    <h2 class="modal-movie__title">${title}</h2>
     <table class="modal-movie__table">
       <tbody class="modal-movie__cell1">
         <tr>
@@ -59,21 +87,21 @@ function renderMovieInfo() {
             Vote / Votes
           </td>
           <td class="modal-movie__cell modal-movie__cell--gray-text">
-            <span class="modal-movie__span modal-movie__span--vote">7.3</span> /
-            <span class="modal-movie__span modal-movie__span--votes">1260</span>
+            <span class="modal-movie__span modal-movie__span--vote">${vote_average}</span> /
+            <span class="modal-movie__span modal-movie__span--votes">${vote_count}</span>
           </td>
         </tr>
         <tr>
           <td class="modal-movie__cell modal-movie__cell--gray-text">
             Popularity
           </td>
-          <td class="modal-movie__cell">100.2</td>
+          <td class="modal-movie__cell">${popularity}</td>
         </tr>
         <tr>
           <td class="modal-movie__cell modal-movie__cell--gray-text">
             Original Title
           </td>
-          <td class="modal-movie__cell">A FISTFUL OF LEAD</td>
+          <td class="modal-movie__cell">${original_title}</td>
         </tr>
         <tr>
           <td
@@ -81,21 +109,12 @@ function renderMovieInfo() {
           >
             Genre
           </td>
-          <td class="modal-movie__cell modal-movie__cell--last-row">Western</td>
+          <td class="modal-movie__cell modal-movie__cell--last-row">${ganresName}</td>
         </tr>
       </tbody>
     </table>
     <h3 class="modal-movie__subtitle">About</h3>
-    <p class="modal-movie__text">
-      Four of the West’s most infamous outlaws assemble to steal a huge stash of
-      gold from the most corrupt settlement of the gold rush towns. But not all
-      goes to plan one is killed and the other three escapes with bags of gold
-      hide out in the abandoned gold mine where they happen across another gang
-      of three – who themselves were planning to hit the very same bank! As
-      tensions rise, things go from bad to worse as they realise the bags of
-      gold are filled with lead... they’ve been double crossed – but by who and
-      how?
-    </p>
+    <p class="modal-movie__text">${overview}</p>
     <ul class="modal-movie__btn-list">
       <li class="modal-movie__btn-item">
         <button
@@ -112,4 +131,19 @@ function renderMovieInfo() {
       </li>
     </ul>
   </div>`;
-}
+  }
+
+  function addModalListeners() {
+    backdrop.addEventListener('click', onCloseModal);
+    window.addEventListener('keydown', onCloseModal);
+    modalMovie.addEventListener('click', onCloseModal);
+  }
+
+  function removeModalListeners() {
+    backdrop.removeEventListener('click', onCloseModal);
+    window.removeEventListener('keydown', onCloseModal);
+    modalMovie.removeEventListener('click', onCloseModal);
+  }
+}, 1000);
+
+export { movieData };
