0ee928b4cf41607ad243baa00938b1809648da7e
BulatovaTati <kohodatat@gmail.com>
2022-10-12 22:03:33 +0300

    add changes in renderPop

--- src/js/dom/reanderTranding.js
+++ src/js/dom/reanderTranding.js
@@ -1,10 +1,10 @@
 import { getPopularData } from '../api/fetchApi';
 import { renderCollection } from './create-movie-markup';
-
 const page = 1;
 
 getPopularData(page)
   .then(data => {
     renderCollection(data.results);
+    localStorage.setItem('input-value', '');
   })
   .catch(console.log);
