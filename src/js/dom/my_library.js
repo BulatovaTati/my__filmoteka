8e9d392c63e72cf27313644d4990bdcb8dcdaadf
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2022-10-13 17:19:23 +0300

    removed unused

--- src/js/dom/my_library.js
+++ src/js/dom/my_library.js
@@ -30,35 +30,6 @@ function renderSavedFilms(name) {
   }
 }
 
-// function cardRendering(result) {
-//   const cardListString = result
-//     .map(el => {
-//       const filmGenresArray = el.genres.map(genreObj => {
-//         return genreObj.name;
-//       });
-//       const title = el.name || el.title;
-//       const year =
-//         el.release_date?.slice(0, 4) || el.first_air_date?.slice(0, 4);
-//       const src = el.poster_path;
-//       const rating = el.vote_average.toFixed(1);
-//       const genre = filmGenresArray.join(', ');
-//       return `<li class="card__item ">
-//       <div class="card__img--container "> <img src="https://image.tmdb.org/t/p/w500${src}" alt="Movie" class="card__img"></div>    
-//                <div class="card__info">
-//           <h2 class="card__title">${title}</h2>
-//          <div class="card__decr">  
-//           <p class="card__genre">${genre} <span> | </span> </p>
-//            <p class="card__year">${year}</p>
-//           <p class="card__rating">${rating}</p>
-//           </div>
-//         </div>
-//       </li>`;
-//     })
-//     .join('');
-
-//   refs.cardsContainer.insertAdjacentHTML('afterbegin', cardListString);
-// }
-
 function setDisabled(el) {
   el.setAttribute('disabled', '');
   el.classList.add('button-active');
