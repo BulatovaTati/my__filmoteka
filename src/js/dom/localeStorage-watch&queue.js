c174a32d23714ac12cf668da33574e70fc6a5e20
vitalik poliakov <vitalik211287@gmail.com>
2022-10-14 12:42:25 +0300

    removed cards from library

--- src/js/dom/localeStorage-watch&queue.js
+++ src/js/dom/localeStorage-watch&queue.js
@@ -1,10 +1,11 @@
-import { refs } from '../common/refs';
+import { isWatchTabActive, refs } from '../common/refs';
+
 
 function localStorageFunction(movieData) {
   const filmObject = JSON.stringify(movieData);
 
   const cartItem = document.querySelector(`[data-id="${movieData.id}"]`);
-  console.log(cartItem, movieData.id);
+//   console.log(cartItem, movieData.id);
 
   const watchBtn = document.querySelector('[data-action="watch"]');
   const queueBtn = document.querySelector('[data-action="queue"]');
@@ -20,61 +21,66 @@ function localStorageFunction(movieData) {
     watchBtn.textContent = 'REMOVE FROM WATCHED';
     // queueBtn.disabled = true
   }
-}
-if (
-  localStorage.getItem('queue').includes(filmObject) &&
-  localStorage.getItem('queue').length > 2
-) {
-  queueBtn.classList.add('button--accent-btn');
-  queueBtn.textContent = 'REMOVE FROM QUEUE';
-  // watchBtn.disabled = true
-}
 
-function addWatch() {
-  if (movieData) {
-    let film = JSON.parse(localStorage.getItem('watch')) || [];
-
-    if (film.find(e => e.id === movieData.id)) {
-      watchBtn.classList.remove('button--accent-btn');
-      watchBtn.textContent = 'ADD TO WATCHED';
-      film = film.filter(e => e.id !== movieData.id);
-      // queueBtn.disabled = false
-    } else {
-      watchBtn.classList.add('button--accent-btn');
-      watchBtn.textContent = 'REMOVE FROM WATCHED';
-      // queueBtn.disabled = true
-      // queueBtn.classList.remove('button--accent-btn')
-      // queueBtn.textContent = 'ADD TO QUEUE'
-      film.push(movieData);
-    }
-
-    localStorage.setItem('watch', JSON.stringify(film));
+  if (
+    localStorage.getItem('queue').includes(filmObject) &&
+    localStorage.getItem('queue').length > 2
+  ) {
+    queueBtn.classList.add('button--accent-btn');
+    queueBtn.textContent = 'REMOVE FROM QUEUE';
+    // watchBtn.disabled = true
   }
-}
 
-function addQueue() {
-  if (movieData) {
-    let film = JSON.parse(localStorage.getItem('queue')) || [];
-    if (film.find(e => e.id === movieData.id)) {
-      queueBtn.classList.remove('button--accent-btn');
-      queueBtn.textContent = 'ADD TO QUEUE';
-      film = film.filter(e => e.id !== movieData.id);
-      // watchBtn.disabled = false;
+  function addWatch() {
+    if (movieData) {
+      let film = JSON.parse(localStorage.getItem('watch')) || [];
 
-      const isLibraryPage = location.pathname.includes('library');
-      if (isLibraryPage && cartItem) {
-        cartItem.remove();
+      if (film.find(e => e.id === movieData.id)) {
+        watchBtn.classList.remove('button--accent-btn');
+        watchBtn.textContent = 'ADD TO WATCHED';
+        film = film.filter(e => e.id !== movieData.id);
+        // queueBtn.disabled = false
+        const isLibraryPage = location.pathname.includes('library');
+        if (isLibraryPage && cartItem && refs.isWatchTabActive) {
+          cartItem.remove();
+        }
+      } else {
+        watchBtn.classList.add('button--accent-btn');
+        watchBtn.textContent = 'REMOVE FROM WATCHED';
+        // queueBtn.disabled = true
+        // queueBtn.classList.remove('button--accent-btn')
+        // queueBtn.textContent = 'ADD TO QUEUE'
+        film.push(movieData);
       }
-    } else {
-      queueBtn.classList.add('button--accent-btn');
-      queueBtn.textContent = 'REMOVE FROM QUEUE';
-      // watchBtn.disabled = true;
-      // watchBtn.classList.remove('button--accent-btn')
-      // watchBtn.textContent = 'ADD TO WATCHED'
-      film.push(movieData);
+
+      localStorage.setItem('watch', JSON.stringify(film));
     }
-    localStorage.setItem('queue', JSON.stringify(film));
+  }
+
+  function addQueue() {
+    if (movieData) {
+      let film = JSON.parse(localStorage.getItem('queue')) || [];
+      if (film.find(e => e.id === movieData.id)) {
+        queueBtn.classList.remove('button--accent-btn');
+        queueBtn.textContent = 'ADD TO QUEUE';
+        film = film.filter(e => e.id !== movieData.id);
+        // watchBtn.disabled = false;
+
+        const isLibraryPage = location.pathname.includes('library');
+          if (isLibraryPage && cartItem && !refs.isWatchTabActive) {
+              console.log(cartItem);
+                   cartItem.remove();
+        }
+      } else {
+        queueBtn.classList.add('button--accent-btn');
+        queueBtn.textContent = 'REMOVE FROM QUEUE';
+        // watchBtn.disabled = true;
+        // watchBtn.classList.remove('button--accent-btn')
+        // watchBtn.textContent = 'ADD TO WATCHED'
+        film.push(movieData);
+      }
+      localStorage.setItem('queue', JSON.stringify(film));
+    }
   }
 }
-
 export { localStorageFunction };
