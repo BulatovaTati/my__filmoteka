ca73416d375e0cb04b5d643270dbaaa0f7d066f1
BulatovaTati <kohodatat@gmail.com>
2022-10-16 17:16:30 +0300

    Update reanderTranding.js

--- src/js/dom/reanderTranding.js
+++ src/js/dom/reanderTranding.js
@@ -2,6 +2,8 @@ import { getPopularData } from '../api/fetchApi';
 import { renderCollection } from './create-movie-markup';
 import { refs } from '../common/refs';
 import { updateLastPaginationPage, pagination } from '../common/pagination';
+import Spinner from '../common/spinner';
+const spinner = new Spinner();
 
 function renderMovieTr() {
   if (refs.input.value !== '') {
@@ -9,13 +11,14 @@ function renderMovieTr() {
   }
 
   const page = 1;
-
+  spinner.enable();
   getPopularData(page)
     .then(data => {
       renderCollection(data.results);
       localStorage.setItem('input-value', '');
       pagination.reset(data.total_pages);
       updateLastPaginationPage(data);
+      spinner.disable();
     })
     .catch(console.log);
 }
